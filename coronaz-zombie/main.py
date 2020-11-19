ff89781e7f11f70e9d975b84e3bdcdc0381af377
cip <ciprianv96@hotmail.it>
2020-11-19 15:58:19 +0200

    working demo with 3 nodes

--- coronaz-zombie/main.py
+++ coronaz-zombie/main.py
@@ -46,41 +46,39 @@ def thread_zombie_listen(kill, zombie, port):
 
 def thread_server_con(kill, zombie, mqtt_server_addr, mqtt_queue):
     # Comment for testing locally
-    #producer = KafkaProducer(bootstrap_servers=[mqtt_server_addr],
-    #                     value_serializer=lambda x:
-    #                     dumps(x).encode('utf-8'))
+    producer = KafkaProducer(bootstrap_servers=[mqtt_server_addr],
+                         value_serializer=lambda x:
+                         dumps(x).encode('utf-8'))
 
     while not kill.wait(1):
         data = zombie.get_next_server_message()
         logging.info("Sending message to server: %s" % data)
 
         # Comment for testing locally
-        # producer.send(mqtt_queue, value=data)
+        producer.send(mqtt_queue, value=data)
 
     logging.info("server con ended")
 
 
 def main(args):
 
-    time.sleep(30)
-
     zombie = Zombie(args['field'], args['position'], args['infected'], args['radius'])
 
     mqtt_server_addr = args['server'][0]
     mqtt_queue = args['server'][1]
 
-    # producer_connection = False
-    # while not producer_connection:
-    #     try:
-    #         producer = KafkaProducer(bootstrap_servers=[mqtt_server_addr],
-    #                         value_serializer=lambda x:
-    #                         dumps(x).encode('utf-8'))
-    #         producer_connection = True
-    #         logging.info("producer online")
-    #         producer.close()
-    #     except:
-    #         logging.info("producer not yet online")
-    #         time.sleep(10)
+    producer_connection = False
+    while not producer_connection:
+        try:
+            producer = KafkaProducer(bootstrap_servers=[mqtt_server_addr],
+                            value_serializer=lambda x:
+                            dumps(x).encode('utf-8'))
+            producer_connection = True
+            logging.info("producer online")
+            producer.close()
+        except:
+            logging.info("producer not yet online")
+            time.sleep(10)
 
     kill = Event()
 
