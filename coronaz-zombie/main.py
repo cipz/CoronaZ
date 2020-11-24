7a91abdaa44b86adf4e3c134e15ff1bd0d9e5740
frensing <frensing@mail.upb.de>
2020-11-24 10:50:04 +0200

    merge main into zombie

--- coronaz-zombie/main.py
+++ coronaz-zombie/main.py
@@ -61,7 +61,6 @@ def thread_server_con(kill, zombie, mqtt_server_addr, mqtt_queue, with_kafka):
 
 
 def main(args):
-
     zombie = Zombie(args['field'], args['position'], args['infected'], args['radius'])
 
     mqtt_server_addr = args['server'][0]
