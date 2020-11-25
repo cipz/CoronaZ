7db1615a1ea3f980bcb288607ef5c1d1eada6359
frensing <frensing@mail.upb.de>
2020-11-25 15:18:29 +0200

    more interesting movement
    zombie will pick random point on game field and walks towards it

--- coronaz-zombie/movement.py
+++ coronaz-zombie/movement.py
@@ -1,29 +1,30 @@
 from random import randint
-import logging
+
 
 def step_gen(zombie):
     while True:
-        direction = randint(0, 3)
-        for i in range(randint(2, 10)):
-            if not valid_step(zombie, direction):
+        goal_position = [randint(0, zombie.field_size[0]), randint(0, zombie.field_size[1])]
+        while True:
+            if at_position(zombie, goal_position):
                 break
+            pos = zombie.position
+            delta = [goal_position[0] - pos[0], goal_position[1] - pos[1]]
+            if abs(delta[0]) > abs(delta[1]):
+                if delta[0] < 0:
+                    direction = 3  # west
+                else:
+                    direction = 1  # east
+            else:
+                if delta[1] < 0:
+                    direction = 2  # south
+                else:
+                    direction = 0  # north
             yield direction
 
-def valid_step(zombie, direction):
-    field = zombie.field_size
+
+def at_position(zombie, goal_position):
     pos = zombie.position
-
-    if direction == 0:
-        pos[0] += 1
-    elif direction == 1:
-        pos[1] += 1
-    elif direction == 2:
-        pos[0] -= 1
-    elif direction == 3:
-        pos[1] -= 1
-
-    if pos[0] < 0 or field[0] < pos[0] or pos[1] < 0 or field[1] < pos[1]:
-        logging.debug('Not valid step: %s' % direction)
-        return False
-
+    for d in [goal_position[0] - pos[0], goal_position[1] - pos[1]]:
+        if d != 0:
+            return False
     return True
