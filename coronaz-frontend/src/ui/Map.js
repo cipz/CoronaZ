86901f426af7245a3dfc81c79aa009e349fbc949
Stefan <vladov@retit.de>
2020-12-05 12:11:41 +0200

    add nodes death

--- coronaz-frontend/src/ui/Map.js
+++ coronaz-frontend/src/ui/Map.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 import * as d3 from "d3";
+import { gray } from 'd3';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -38,15 +39,21 @@ export default function Map(props) {
         
         if(node) {
           Object.entries(node).forEach(([key, value]) => {
-            if(key !== "_id") {
-              console.log(value)
+            if(key != "_id") {
+              console.log(value);
+
+              let fill = "gray";
+              if(value.alive) {
+                fill = (value.infected)? "red": "blue";
+              }
+
               svg.append("circle")
-                .attr("cx", value[0][0] * scaleFactor)
-                .attr("cy", value[0][1] * scaleFactor)
+                .attr("cx", value.position[0] * scaleFactor)
+                .attr("cy", value.position[1] * scaleFactor)
                 .attr("r", radius)
-                .attr("fill", (value[1])? "red": "blue");
+                .attr("fill", fill);
             }
-          });
+          }) 
         }
     });
 
